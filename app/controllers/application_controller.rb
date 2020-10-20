05fce214f26fad4d286635310f937cb408ce2d9a
Kylan Wong <mynameiskylan@gmail.com>
2020-10-20 11:57:21 -0700

    add posts scaffold, navbar partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :birthday, :location, :profile_picture])
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :birthday, :location, :email, :password, :password_confirmation, :remember_me, :profile_picture, :profile_picture_cache) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :birthday, :location, :password, :password_confirmation, :current_password, :profile_picture, :profile_picture_cache, :remove_profile_picture) }
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,20 @@
 class User < ApplicationRecord
+  mount_uploader :profile_picture, ProfilePictureUploader
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  mount_uploader :profile_picture, ProfilePictureUploader
+  
+  # User Profile Picture Validation
+  validates_integrity_of :profile_picture
+  validates_processing_of :profile_picture
+
+  has_many :posts
+
+  private
+
+  def avatar_size_validation
+    errors[:avatar] << "should be less than 500KB" if avatar.size > 0.5.megabytes
+  end
 end
