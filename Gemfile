a3cfcfd649b4c6067912e4147b057b37bdb89835
Kylan Wong <mynameiskylan@gmail.com>
2020-10-22 13:09:22 -0700

    switch carrierwave storage to aws

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'carrierwave'
 gem 'mini_magick'
 gem 'omniauth-facebook'
 gem 'figaro'
+gem 'carrierwave-aws'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :birthday, :location, :email, :password, :password_confirmation, :remember_me, :profile_picture, :profile_picture_cache) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :birthday, :location, :password, :password_confirmation, :current_password, :profile_picture, :profile_picture_cache, :remove_profile_picture) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :birthday, :location, :email, :password, :password_confirmation, :current_password, :profile_picture, :profile_picture_cache, :remove_profile_picture) }
   end
 end
