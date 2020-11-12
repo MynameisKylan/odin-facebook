6e3938044512450b075afa9a226bfe48647f3bd2
Kylan Wong <mynameiskylan@gmail.com>
2020-11-12 15:03:38 -0800

    add validation for duplicate friendships, modify condition for showing user location

---
+++ app/validators/friendship_validator.rb
@@ -0,0 +1,9 @@
+class FriendshipValidator < ActiveModel::Validator
+  def validate(friendship)
+    user = User.find(friendship.user_id)
+    friend = User.find(friendship.friend_id)
+    if user.friends.find(friendship.friend_id)
+      friendship.errors[:friends] << "You are already friends with #{friend.first_name}."
+    end
+  end
+end
